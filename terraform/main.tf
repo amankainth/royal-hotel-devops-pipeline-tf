1ac4f726787310a0deb5c076602e1bf85901e908
amankainth <aman.kainth@gmail.com>
2026-07-13 20:17:04 +0530

    Fetch default VPC and subnets, update instance resource
    Added data sources to fetch default VPC and its subnets. Updated instance resource to include subnet ID.

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,26 @@ data "aws_ami" "amazon_linux_2023" {
   }
 }
 
+# 1. Fetch the default VPC explicitly
+data "aws_vpc" "default" {
+  default = true
+}
+
+# 2. Fetch subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 # Create Security Group for Server 2 (Webserver)
 resource "aws_security_group" "webserver_sg" {
   name        = "royal-hotel-webserver-sg"
   description = "Allow SSH and Java Web UI Traffic"
+  
+  # EXPLICITLY BIND VPC ID HERE:
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -40,9 +56,12 @@ resource "aws_security_group" "webserver_sg" {
 resource "aws_instance" "dev_instance" {
   ami                         = data.aws_ami.amazon_linux_2023.id
   instance_type               = var.instance_type
-  key_name                    = var.key_name # <--- Injects the SSH public key
-  vpc_security_group_ids      = [aws_security_group.webserver_sg.id] # <--- Attaches the security group
-  associate_public_ip_address = true # Ensures public IP assignment
+  key_name                    = var.key_name
+  
+  # Attach explicitly fetched VPC Security Group ID and Subnet ID
+  vpc_security_group_ids      = [aws_security_group.webserver_sg.id]
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  associate_public_ip_address = true
 
   tags = {
     Name        = "royal-hotel-dev-instance"
